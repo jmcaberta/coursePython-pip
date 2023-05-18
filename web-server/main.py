8937bbc4935f9ea575fc6bc630cdd4a189f7b2ce
Juan Miguel <caber_t@hotmail.com>
2023-05-18 18:05:06 +0200

    Testing FastApi

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get("/")   
+
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return """
+        <h1>Test of a web page</h1>
+        <p>this a parraf</p>
+    """
 
 def run():
     store.get_categories()
